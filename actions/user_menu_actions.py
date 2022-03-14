7f2fb51534dbad5b678e0efac732a4fb9922aa4a
Amy Do <ahd6901@rit.edu>
2022-03-14 18:44:15 -0400

    queries for inspect_available_tools, inspect_lent_tools inspect_borrowed_tools

--- actions/user_menu_actions.py
+++ actions/user_menu_actions.py
@@ -21,17 +21,51 @@ def show_stats(args):
   print('Action: Show Stats')
   print(args)
 
+# The list of available tools must be ordered by name alphabetically
 def inspect_available_tools(args):
-  #REQ: 10 a
-  print('Action: inspect_available_tools')
-  print(args)
+    # REQ: 10 a
+    print('Action: inspect_available_tools')
+    tuples = exec_get_all('SELECT name FROM tools WHERE available is True ORDER BY name ASC;')
+    [print(t[0]) for t in tuples]
+
 
 def inspect_lent_tools(args):
-  # REQ:10 b
-  print('Action: inspect_lent_tools')
-  print(args)
+    # REQ:10 b
+    print('Action: inspect_lent_tools')
+
+    tuples = exec_get_all(
+        "SELECT r.tool_id, t.name, u.username as holder, r.overdue, r.date_borrowed,r.date_returned "
+        "FROM user_tool_requests r "
+        "Inner JOIN users u ON r.requesting_user_id= u.user_id "
+        "INNER JOIN tools t on t.tool_id = r.tool_id "
+        "WHERE r.status ='accepted' AND r.date_borrowed is not null AND date_returned is null "  
+        "ORDER BY r.date_borrowed ASC;")
+    print('Total found:', len(tuples))
+
+    for tup in tuples:
+        # if tup[5]:
+        #     current_holder = exec_get_one("SELECT u.username as owner FROM "
+        #                                   "catalog_tools c INNER JOIN users u ON "
+        #                                   "u.user_id= c.owner_id WHERE c.tool_id={0};".format(tup[0]))[0]
+        # else:
+        #     current_holder= tup[2]
+        print(
+            "Tool:{0} | holder:{1} | overdue:{2} | date borrowed:{3}".format(tup[1], tup[2],
+                                                                                                 str(tup[3]),
+                                                                                                 str(tup[4]),
+                                                                                                 str(tup[5])))
+
 
 def inspect_borrowed_tools(args):
-  # REQ:10 c
-  print('Action: inspect_borrowed_tools')
-  print(args)
+    # REQ:10 c
+    print('Action: inspect_borrowed_tools')
+    tuples = exec_get_all("SELECT t.name, u.username as tool_owner, r.overdue, r.date_borrowed "
+                          "FROM user_tool_requests r "
+                          "INNER JOIN catalog_tools c ON r.tool_id=c.tool_id "
+                          "INNER JOIN users u ON c.owner_id=u.user_id "
+                          "INNER JOIN tools t on t.tool_id = r.tool_id "
+                          "WHERE r.status='accepted' AND r.date_borrowed is not null "   
+                          "ORDER BY r.date_borrowed ASC;")
+    print('Total found:', len(tuples))
+    [print("Tool:{0} | owner:{1} | overdue:{2} | date borrowed:{3} ".format(t[0], t[1], str(t[2]), str(t[3]))) for t in
+     tuples]
