b26613e46b6f70b1364a1ad6ae28f584f660cc21
NathanGilbert6917 <54862260+NathanGilbert6917@users.noreply.github.com>
2022-03-20 23:04:02 -0400

    Catalog actions (#4)
    * Added actions for returning, adding, and removing tools from catalog
    
    * Added searching, updating actions
    
    * Added add_tool functionality
    
    * toolid autoserializes now
    
    * Added return tool funtionality, requests menu almost done
    
    Co-authored-by: ritwam2134 <wam2134@g.rit.edu>
    Co-authored-by: Wilson McDade <wam2134@rit.edu>

--- actions/catalog_menu_actions.py
+++ actions/catalog_menu_actions.py
@@ -1,4 +1,10 @@
+from json import tool
+from sre_constants import CATEGORY_WORD
+from unicodedata import category
 from db_utils import exec_get_one, exec_get_all, exec_commit
+from re import search, findall
+from datetime import date
+import random
 
 
 def action_input_map():
@@ -22,29 +28,219 @@ def action_input_map():
 def add_tool(args, userid):
   # REQ:2
   print('Action: Add Tool')
-  print(args, userid)
+
+  today = date.today()
+  d = today.strftime("%Y-%m-%d")
+
+  name = input("Tool Name: ")
+  desc = input("Description of tool: ")
+  price = round(float(input("Tool Price: ")),2)
+  shareinput = input("Sharable? (t/f): ")
+  availinput = input("Available to share? (t/f): ")
+
+
+  if shareinput == "t":
+    sharable = True
+  elif shareinput == "f":
+    sharable = False
+  else:
+    print("Must choose t or f")
+    return
+
+  if availinput == "t":
+    available = True
+  elif availinput == "f":
+    available = False
+  else:
+    print("Must choose t or f")
+    return
+
+  try:
+
+    exec_commit("INSERT INTO tools(times_lent,barcode,name,description,shareable,purchase_price,purchase_date,available) VALUES\
+      ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}');".format(0,random.randint(0,999999999),name,desc,sharable,price,d,available))
+
+    max_toolid = int(exec_get_one("SELECT MAX(tool_id) from tools")[0])
+    
+    exec_commit("INSERT INTO catalog_tools(owner_id, tool_id) VALUES ('{0}', '{1}');".format(userid,max_toolid))
+    print("Tool Added to catalog!")
+  except Exception as e:
+    print('Error Adding Tool to Catalog', e)
+    return
+
+  return
 
 def sort_tools(args, userid):
   # REQ:5
   print('Action: Sort Tools')
+  print("usage: sort_tools")
   print(args)
+
+  tuples = []
+
+  colinput = input("Name or Category? (name/category/tool_id): ")
+  sortinput = input("Ascending or Descending? (ASC/DESC): ")
+
+  tuples = exec_get_all("SELECT tool_id, name, description FROM tools ORDER BY {0} {1};".format(colinput,sortinput))
+  cols = ["tool_id","name","description"]
+  print_tuples(cols,tuples)
+
 
 def remove_tool(args, userid):
   # REQ:2 / 12
   print('Action: Remove Tool')
   print(args)
 
+  toolid = input("ID of tool to remove: ")
+
+  tool_exists = exec_get_one("SELECT COUNT(tool_id) FROM tools WHERE tool_id='{0}'".format(toolid))
+
+  if tool_exists:
+    try:
+      exec_commit("DELETE FROM catalog_tools WHERE tool_id = '{0}';".format(toolid))
+      print("Tool Removed from Catalog!")
+    except Exception as e:
+      print('Error Removing Tool From Catalog', e)
+      return
+  else:
+    print("Tool does not exist")
+
+  return
+
 def update_tool(args, userid):
   # REQ:2
   print('Action: Update Tool')
-  print(args)
+  print('usage: update_tool <tool_id>')
+
+  if len(args) != 2:
+    print("Incorrect amount of arguments")
+    return
+
+  tool_exists = exec_get_one("SELECT COUNT(tool_id) FROM tools WHERE tool_id='{0}'".format(args[1]))
+
+  if tool_exists:
+    try:
+
+      tool = exec_get_one("SELECT tool_id,times_lent,barcode,name,description,shareable,purchase_price,purchase_date,available from tools where tool_id='{0}';".format(args[1]))
+      print_tuple(["tool_id","times_lent","barcode","name","description","shareable","purchase_price","purchase_date","available"], tool)
+
+      cols = ["times_lent","barcode","name","description","shareable","purchase_price","purchase_date","available"]
+
+      print("times_lent, barcode, name, description, sharable, purchase_price, purchase_date, available")
+      col = input("Column to update: ")
+
+      if col in cols:
+        val = input("Value: ")
+        exec_commit("UPDATE tools SET {0} = '{1}' where tool_id = {2}".format(col,val,int(args[1])))
+        print("Updated tool")
+
+        tool = exec_get_one("SELECT tool_id,times_lent,barcode,name,description,shareable,purchase_price,purchase_date,available from tools where tool_id='{0}';".format(args[1]))
+        print_tuple(["tool_id","times_lent","barcode","name","description","shareable","purchase_price","purchase_date","available"], tool)
+
+        return
+      else:
+        print("Choose from available columns to set")
+    except Exception as e:
+      print('Error Updating tool', e)
+      return
+  else:
+    print("Tool does not exist")
+    return
+
 
 def search_tool(args, userid):
   # REQ:4
   print('Action: Search Tool')
-  print(args)
+
+  search_terms = []
+
+  if len(args) > 1:
+    if args[1].lower() == "name" or args[1].lower() == "category":
+      for j in range(2,len(args)):
+        search_terms.append(args[j].capitalize())
+        search_terms.append(args[j].lower())
+        print("added search term {0}".format(args[j]))
+    elif args[1] == "barcode":
+      search_terms.append(args[2])
+    else:
+      print("usage: search_tool <name/barcode/category> <query> (showall)")
+      return
+  else:
+    print("usage: search_tool <name/barcode/category> <query> (showall)")
+    return
+
+  tuples = []
+
+  if args[1].lower() == "name":
+    cols = ["tool_id", "name", "description"]
+    tuples = exec_get_all("SELECT tool_id, name, description from tools where name like '%%_{0}%%'".format(search_terms[0][1:]))
+    print_tuples(cols,tuples)
+
+  elif args[1].lower() == "category":
+    category_tup = exec_get_all("SELECT category_id, name from categories where name like '%%_{0}%%'".format(args[2]))
+    print("Getting tools in category {0}".format(category_tup[0][1]))
+    tools_in_category = exec_get_all("SELECT tool_id from categorized_tools where category_id = '{0}'".format(category_tup[0][0]))
+    for tool in tools_in_category:
+      tuples.append(exec_get_one("SELECT tool_id, name, description from tools where tool_id = '{0}'".format(int(tool[0]))))
+  
+    tuples = list(set(tuples) | set(tuples))
+    cols = ["tool_id","name","description"]
+    print_tuples(cols,tuples)
+
+  else:
+    cols = ["tool_id", "name", "description"]
+    tuples = exec_get_all("SELECT tool_id, name, description from tools where barcode = '{0}'".format(search_terms[0]))
+    print_tuples(cols,tuples)
 
 def return_tool(args, userid):
   # REQ: 11
   print('Action: Return Tool')
   print(args)
+
+  today = date.today()
+  d = today.strftime("%Y-%m-%d")
+
+  toolid = input("ID of Tool to return: ")
+
+  tool_exists = exec_get_one("SELECT COUNT(tool_id) FROM tools WHERE tool_id='{0}';".format(toolid))
+
+  if tool_exists:
+    try:
+      exec_commit("UPDATE user_tool_requests SET date_returned = '{0}', date_status_changed = '{1}' WHERE tool_id = '{2}' and requesting_user_id = '{3}';".format(d,d,toolid,userid))
+      print("Tool Returned!")
+    except Exception as e:
+      print('Error Returning Tool', e)
+      return
+  else:
+    print("Tool does not exist")
+
+  return
+
+def print_tuples(cols,tuples):
+  for col in cols:
+    print(col+"\t\t\t",end="")
+  print('\n')
+  for tup in tuples:
+    for val in tup:
+      if len(str(val)) < 4:
+        print(str(val)+"\t\t", end="")
+      elif len(str(val)) < 23:
+        print(str(val)+"\t\t\t", end="")
+      else:
+        print(str(val)[:27]+"...\t\t",end="")
+    print("\n")
+
+
+def print_tuple(cols,tuple):
+  for col in cols:
+    print(col+"\t\t\t",end="")
+  print('\n')
+  for val in tuple:
+    if len(str(val)) < 4:
+      print(str(val)+"\t\t", end="")
+    elif len(str(val)) < 23:
+      print(str(val)+"\t\t\t", end="")
+    else:
+      print(str(val)[:27]+"...\t\t",end="")
+  print("\n")
+
--- actions/requests_menu_actions.py
+++ actions/requests_menu_actions.py
@@ -1,5 +1,6 @@
+from urllib import request
 from db_utils import exec_get_one, exec_get_all, exec_commit
-
+from datetime import date
 
 def action_input_map():
   input_map = {
@@ -10,8 +11,8 @@ def action_input_map():
     'goto requests menu': 'goto requests menu',
     'goto category menu': 'goto category menu',
     'goto catalog menu': 'goto catalog menu',
-    'respond_to_request': 'respond_to_request <request_id> <response>',
-    'request_tool': 'request_tool <tool_id> <user_id> <duration>',
+    'respond_to_request': 'respond_to_request',
+    'request_tool': 'request_tool',
     'manage_requests': 'manage_requests'
   }
   return input_map
@@ -21,12 +22,87 @@ def respond_to_request(args, userid):
   print('Action: Respond to Request')
   print(args)
 
+
+  requesting_users = []
+  incoming_req_tuples = get_incoming_requests(userid)
+  if len(incoming_req_tuples) != 0:
+    for tuple in incoming_req_tuples:
+      requesting_users.append(tuple[0])
+    
+    print_requests(userid,True)
+  else:
+    print("No Requests to respond to")
+    return
+
+  print(requesting_users)
+
+  today = date.today()
+  d = today.strftime("%Y-%m-%d")
+
+  reqid = int(input("ID of user to respond to: "))
+  status = input("Response to request (accepted/declined): ")
+
+  if reqid in requesting_users:
+    try:
+      exec_commit("UPDATE user_tool_requests SET status = '{0}', date_status_changed = '{1}' WHERE requesting_user_id = '{2}'".format(status,d,reqid))
+      print("Updated Status")
+    except Exception as e:
+      print("Error updating request", e)
+      return
+  if len(requesting_users) > 1:
+    print_requests(userid,True)
+  
+
 def request_tool(args, userid):
   # REQ:6
   print('Action: Request Tool')
   print(args)
 
+  today = date.today()
+  d = today.strftime("%Y-%m-%d")
+
+  toolid = input("ID of Tool to borrow")
+  reqdate = input("Date required (yyyy-mm-dd): ")
+  returndate = input("Date to return (yyyy-mm-dd): ")
+
+  try:
+    exec_commit("INSERT INTO user_tool_requests(requesting_user_id,tool_id,date_required,status,overdue,date_status_changed,duration,expected_return_date) VALUES ({0},{1},'{2}','pending',false,'{3}',1,'{4}');".format(userid,toolid,reqdate,d,returndate))
+    print("Request Added")
+  except Exception as e:
+    print("Error adding request", e)
+    return
+
 def manage_requests(args, userid):
   # REQ:7
   print('Action: Request Tool')
   print(args)
+
+  print("Incoming requests:")
+  print_requests(userid,True)
+  print("Outgoing requests:")
+  print_requests(userid,False)
+
+def get_incoming_requests(userid):
+  request_tuple = exec_get_all("SELECT r.requesting_user_id, t.name, r.date_required, r.status, r.expected_return_date,r.date_status_changed from user_tool_requests r inner join tools t on t.tool_id = r.tool_id inner join catalog_tools c on t.tool_id = c.tool_id where c.owner_id = '{0}' and r.status = 'pending'".format(userid))
+  return request_tuple
+
+def get_outgoing_requests(userid):
+  request_tuple = exec_get_all("SELECT r.requesting_user_id, t.name, r.date_required, r.status, r.expected_return_date, r.date_status_changed from user_tool_requests r inner join tools t on t.tool_id = r.tool_id where r.requesting_user_id = '{0}'".format(userid))
+  return request_tuple
+
+def print_requests(userid,incoming):
+  # get open requests for a user's tools
+
+  if incoming:
+    request_tuple = get_incoming_requests(userid)
+  else:
+    request_tuple = get_outgoing_requests(userid)
+
+  print("Requester ID \tTool Name \t\tDate Required\t\t Status \t\t Expected Return Date \t\t Last Status Change")
+
+  for tuple in request_tuple:
+    for val in tuple:
+      print(str(val)[:18], end="\t\t")
+
+    print("\n")
+
